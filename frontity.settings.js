c348ce5d0178abcd2fa42fac9a34e88f1ea8b9b3
Olaf Lindström <olaf.lindstrom@angrycreative.se>
2020-10-07 12:26:36 +0200

    Add support for multiple sites

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,8 +1,10 @@
-const settings = {
-  "name": "knowitfrontity",
+const settings = [
+{
+  "name": "site1",
+  "match": [".*strandbergguitars.com\/eu(\/.*)?$"], 
   "state": {
     "frontity": {
-      "url": "https://test.frontity.org",
+      "url": "https://strandbergguitars.com/eu",
       "title": "Test Frontity Blog",
       "description": "WordPress installation for Frontity development"
     }
@@ -45,13 +47,70 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "api": "https://test.frontity.org/wp-json"
+          "api": "https://strandbergguitars.com/eu/wp-json"
         }
       }
     },
     "@frontity/tiny-router",
     "@frontity/html2react"
   ]
-};
+},
+{
+  "name": "site2",
+  "match": [".*strandbergguitars.com\/jp(\/.*)?$"], 
+  "state": {
+    "frontity": {
+      "url": "https://strandbergguitars.com/jp",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://strandbergguitars.com/jp/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+}
+];
 
 export default settings;
