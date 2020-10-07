9a914a0f3b2de000e5b8f2dfdc55cb3a6d632efe
Olaf Lindström <olaf.lindstrom@angrycreative.se>
2020-10-07 12:04:01 +0200

    Add menu handler to project

--- packages/mars-theme/src/index.js
+++ packages/mars-theme/src/index.js
@@ -2,6 +2,37 @@ import Theme from "./components";
 import image from "@frontity/html2react/processors/image";
 import iframe from "@frontity/html2react/processors/iframe";
 
+const menuHandler = {
+  name: "menus",
+  priority: 10,
+  pattern: "menus/:id",
+  func: async ({ route, params, state, libraries }) => {
+    const { api } = libraries.source;
+    const { id } = params;
+
+    // 1. fetch the data you want from the endpoint page
+    const response = await api.get({
+      endpoint: "menu-items",
+      params: {
+        menus: id,
+        per_page: 100 // To make sure we get all elements
+      }
+    });
+
+    // 2. get an array with each item in json format
+    const items = await response.json();
+
+    // 3. add data to source
+    const currentPageData = state.source.data[route];
+
+    Object.assign(currentPageData, {
+      id,
+      items,
+      isMenu: true
+    });
+  }
+};
+
 const marsTheme = {
   name: "@frontity/mars-theme",
   roots: {
@@ -47,6 +78,9 @@ const marsTheme = {
        */
       processors: [image, iframe],
     },
+    source: {
+      handlers: [menuHandler]
+    }
   },
 };
 
